f2d2469b4834955142b339c97a99e62e88771c77
AnuragPandey01 <anuragpandey87187@gmail.com>
2024-03-11 15:17:38 +0530

    cleanup

--- app/src/main/java/com/example/smplplayer/ui/screens/MusicListDisplay.kt
+++ app/src/main/java/com/example/smplplayer/ui/screens/MusicListDisplay.kt
@@ -7,29 +7,6 @@ import androidx.compose.ui.Modifier
 import com.example.smplplayer.data.MusicItem
 import com.example.smplplayer.ui.components.MusicListItem
 
-/*@Composable
-fun HomeScreen(
-    musicList: List<MusicItem>,
-    mediaController: MediaController
-) {
-
-    LaunchedEffect(Unit){
-        mediaController.addMediaItems(musicList.map { it.toMediaItem() })
-    }
-
-    LazyColumn{
-        items(
-            musicList,
-            key = { it.id }
-        ){mi->
-            MusicListItem(mi){
-                mediaController.seekTo(musicList.indexOfFirst { it.id == mi.id }, 0)
-                mediaController.play()
-            }
-        }
-    }
-}*/
-
 @Composable
 fun MusicListDisplay(
     musicList: List<MusicItem>,
